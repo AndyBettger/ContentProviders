a346cc53ab3f2f411196761818ed94a96c6c05d6
andybettger <andy@bettger.net>
2013-08-28 19:34:03 +0100

    Added button listener and query contacts code

--- src/sixth/example/contentproviders/MainActivity.java
+++ src/sixth/example/contentproviders/MainActivity.java
@@ -1,8 +1,14 @@
 package sixth.example.contentproviders;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.provider.ContactsContract;
+import android.database.Cursor;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +16,22 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button queryButton = (Button)findViewById(R.id.button1);
+		queryButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				queryContact ();
+			}
+		});
+	}
+
+	private void queryContact() {
+		Cursor nameCursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+		while (nameCursor.moveToNext()) {
+			String contactName = nameCursor.getString(nameCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY));
+			Toast.makeText(this, contactName, Toast.LENGTH_SHORT).show();
+		}
+		nameCursor.close();
+		
 	}
 
 	@Override
